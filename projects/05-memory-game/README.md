0ac5dcfd67e754b1b74d8ccfb37d09fc74a1178a
Pablo <pablo.alv.zal@gmail.com>
2018-04-15 09:58:07 +0200

    first submission memory game

--- projects/05-memory-game/README.md
+++ projects/05-memory-game/README.md
@@ -3,10 +3,13 @@
 ## Table of Contents
 
 * [Instructions](#instructions)
-* [Contributing](#contributing)
 
 ## Instructions
 
-... write instructions...
+1. Match cards with identical icons
+2. You have unlimited number of moves. However, the more moves you take, the less final stars you will have, and the lower your score.
+3. You will also be timed, but this does not affect your score!
 
-For specific, detailed instructions, look at the project instructions in the [Udacity Classroom](https://classroom.udacity.com/me).
+## Implements
+
+This game is made only with JavaScript (no jQuery was used). Adaptations to functions found on W3Schools and stackoverflow.com are linked in the source code, which you can see via the browser developer tools.  
--- projects/05-memory-game/js/app.js
+++ projects/05-memory-game/js/app.js
@@ -7,6 +7,22 @@ let icons = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt",
   "fa-cube", "fa-bicycle", "fa-leaf", "fa-bomb"
   ];
 
+// Global tracking variables
+let score;
+let moves;
+let matchedCards;
+let cardChoice;
+let pairs;
+let delay = (function() {
+	let timer = 0;
+	return function(callback, ms) {
+		clearTimeout(timer);
+		timer = setTimeout(callback, ms);
+	};
+})();
+const moveElement = document.getElementById('moves');
+const starElement = document.getElementById('stars');
+
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the "shuffle" method below
@@ -18,8 +34,9 @@ const restart = document.getElementById('restart');
 restart.addEventListener('click', startGame);
 
 function startGame() {
-	// Dont flush for debug
-	// icons = shuffle(icons);
+	resetGame();
+	// Dont shuffle for debug
+	icons = shuffle(icons);
 	const deck = document.getElementById("deck");
 	while (deck.firstChild) {
 		deck.removeChild(deck.firstChild);
@@ -37,7 +54,6 @@ function startGame() {
 	}
 }
 
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
 	var currentIndex = array.length,
@@ -51,24 +67,11 @@ function shuffle(array) {
 		array[currentIndex] = array[randomIndex];
 		array[randomIndex] = temporaryValue;
 	}
-
 	return array;
 }
-let score = 0;
-let moves = 0;
-let matchedCards = [];
-let cardChoice;
-let pairs = [];
-let delay = (function() {
-	let timer = 0;
-	return function(callback, ms) {
-		clearTimeout(timer);
-		timer = setTimeout(callback, ms);
-	};
-})();
 
 // Event delegation
-// Use ".target" to conect to each card
+// Use ".target" (bubbling up) to conect to each card
 deck.addEventListener('click', function flipCard(e) {
 	if (e.target.nodeName === 'LI' && pairs.length < 2) {
 		cardChoice = e.target;
@@ -81,17 +84,16 @@ deck.addEventListener('click', function flipCard(e) {
 			incrementMove();
 			if (pairs[0] === pairs[1]) {
 				addMatchToScore();
-				checkWin();
 				pairs.length = 0; // Clear array
-				console.log("pairs: " + pairs);
 
 			} else {
 				delay(function(cardChoice) {
 					let incorrectPair = deck.getElementsByClassName('open');
 					incorrectPair[1].classList.remove('open');
 					incorrectPair[0].classList.remove('open');
+					pairs.length = 0; // Clear array
 				}, 1000);
-				pairs.length = 0; // Clear array
+
 			}
 		}
 
@@ -100,41 +102,125 @@ deck.addEventListener('click', function flipCard(e) {
 
 function addMatchToScore() {
 	let correctElementPair = deck.getElementsByClassName('open');
-	Array.prototype.forEach.call(correctElementPair, element => {
-		element.classList.add('match');
-		matchedCards.push(element.classList);
-		score = matchedCards.legth / 2;
-	});
-	Array.prototype.forEach.call(correctElementPair, element => {
-		element.classList.remove('open');
-	});
+	++matchedCards;
+	score = matchedCards / 2;
+	correctElementPair[0].classList.add('match');
+	correctElementPair[1].classList.add('match');
+	correctElementPair[1].classList.remove('open');
+	correctElementPair[0].classList.remove('open');
+	checkWin();
 }
 
-const moveElement = document.getElementById('moves');
+
 moveElement.textContent = moves.toString();
 
 function incrementMove() {
+	// increment the move counter and display it on the page
 	moves += 1;
 	moveElement.textContent = moves.toString();
 
-	// increment the move counter and display it on the page
-}
-
-// TODO: follow Modal link to prepare a modal message!
-function checkWin() {
-	if (matchedCards.length === 16) {
-		// Display win message with the final score
-		// https://www.w3schools.com/howto/howto_css_modals.asp
+	if (moves === 10) {
+		starElement.removeChild(starElement.getElementsByTagName('li')[0]);
+	} else if (moves === 17) {
+		starElement.removeChild(starElement.getElementsByTagName('li')[0]);
+	} else if (moves === 25) {
+		starElement.removeChild(starElement.getElementsByTagName('li')[0]);
 	}
 }
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+
+var minutesLabel = document.getElementById("minutes");
+var secondsLabel = document.getElementById("seconds");
+var start;
+var totalSeconds;
+var modalMin = document.getElementById("totmin");
+var modalSec = document.getElementById("totsec");
+
+function resetGame() {
+	endTimer();
+	moves = 0;
+	moveElement.textContent = moves.toString();
+	// Insert missing stars
+	while (starElement.getElementsByTagName('li').length < 3) {
+		let star = document.createElement("LI");
+		let icon = document.createElement("I");
+		icon.classList.add('fa', 'fa-star');
+		star.appendChild(icon);
+		starElement.appendChild(star);
+	}
+	score = 0;
+	matchedCards = 0;
+	pairs = [];
+	startTimer();
+
+}
+
+function startTimer() {
+	// Timer, adapted from https://stackoverflow.com/a/5517836/5118344
+	totalSeconds = 0;
+	start = setInterval(setTime, 1000); // Call function every 1s
+
+	function setTime() {
+		++totalSeconds;
+		secondsLabel.innerHTML = digitalClock(totalSeconds % 60);
+		minutesLabel.innerHTML = digitalClock(parseInt(totalSeconds / 60));
+	}
+}
+
+function digitalClock(val) {
+	let valString = val + "";
+	if (valString.length < 2) {
+		return "0" + valString;
+	} else {
+		return valString;
+	}
+}
+
+function endTimer() {
+	clearInterval(start);
+}
+
+function checkWin() {
+	console.log(matchedCards);
+	if (matchedCards === 8) {
+		// Display win message with the final score
+		const finalScore = (matchedCards * 2 *
+			starElement.getElementsByTagName('li').length);
+
+		let score = document.getElementById("score");
+		score.textContent = finalScore.toString();
+
+		modalSec.innerHTML = digitalClock(totalSeconds % 60);
+		modalMin.innerHTML = digitalClock(parseInt(totalSeconds / 60));
+		endTimer();
+
+		// Get the modal
+		// https://www.w3schools.com/howto/howto_css_modals.asp
+		let modal = document.getElementById('myModal');
+		// Get the <span> and <buttton> element that closes the modal and/or restart game.
+		let close = document.getElementsByClassName("close")[0];
+
+		let playBtn = document.getElementById('play');
+
+
+		modal.style.display = "flex";
+
+		// When the user clicks on <span> (x), close the modal
+		close.onclick = function() {
+			modal.style.display = "none";
+		}
+		playBtn.onclick = function() {
+			modal.style.display = "none";
+			startGame();
+
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+
+	}
+}
